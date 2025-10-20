7514b7121b9c8b21bc4a7e3b07dffd99d3f8bb8b
Nikita Khlivnyi <9099017@stud.op.edu.ua>
2025-10-20 13:01:31 +0300

    .

--- src/main/java/ua/opnu/Point3D.java
+++ src/main/java/ua/opnu/Point3D.java
@@ -32,7 +32,8 @@ public class Point3D extends Point {
         long dx = (long) getX() - p.getX();
         long dy = (long) getY() - p.getY();
         long dz = (long) z - p.z;
-        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+        double d = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        return Math.round(d * 1e13) / 1e13;
     }
 
     @Override
@@ -40,7 +41,8 @@ public class Point3D extends Point {
         long dx = (long) getX();
         long dy = (long) getY();
         long dz = (long) z;
-        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+        double d = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        return Math.round(d * 1e13) / 1e13;
     }
 
     @Override
